7eafd76ddd4e1186f52cf41f9e5e18ac5b6ef52d
wesley <menglz2128@outlook.com>
2021-10-25 22:44:21 +0800

    Update val.py

--- val.py
+++ val.py
@@ -1,6 +1,6 @@
-
+from PIL.Image import Image
 import torch
-from torch import optim
+import matplotlib.pyplot as plt
 from torch.utils.data import DataLoader
 from torchvision.transforms import transforms
 
@@ -14,18 +14,36 @@ data_transform = transforms.Compose([
     transforms.Normalize([0.5], [0.5])
 ])
 
-if __name__ == '__main__':
-    model = Net()
-    model.load_state_dict(torch.load(r"weight\weights_a_19.pth", map_location='cpu'))
-    GGN_dataset = GGNDataset("dataset", transform=data_transform)
-    dataloaders = DataLoader(GGN_dataset)
-    model.eval()
-    import matplotlib.pyplot as plt
+
+def show_result(model, dataloader):
     plt.ion()
     with torch.no_grad():
-        for x, y, _ in dataloaders:
+        for x, y, _ in dataloader:
             output = model(x, y)
             output = torch.squeeze(output).numpy()
             plt.imshow(output)
             plt.pause(0.01)
         plt.show()
+
+
+def get_IoU(model, dataloader):
+    for x, y, mask in dataloader:
+        mask = torch.squeeze(mask).numpy()
+        output = model(x, y)
+        output = torch.squeeze(output).detach().numpy()
+        output = output > 0.5
+        mask = mask > 0.5
+        intersection = (output & mask).sum()
+        union = (output | mask).sum()
+        print((intersection) / union)
+
+
+if __name__ == '__main__':
+    # 加载模型
+    model = Net()
+    model.load_state_dict(torch.load(r"weight\weights_a_19.pth", map_location='cpu'))
+    GGN_dataset = GGNDataset("dataset", transform=data_transform)
+    dataloader = DataLoader(GGN_dataset)
+    model.eval()
+    #show_result(model, dataloader)
+    get_IoU(model, dataloader)
