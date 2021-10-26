c027b94260059f7cea4ede94959b54e0e94e2b36
wesley <menglz2128@outlook.com>
2021-10-26 15:34:02 +0800

    update dataset

--- preprocess.py
+++ preprocess.py
@@ -12,8 +12,15 @@ PATH = "D:\Dataset"
 tables = pd.read_csv(r"coordinates\GGN_original.csv")
 
 
+def maxmin_normalize(x):
+    min = np.min(x)
+    max = np.max(x)
+    x = (x - min) / (max - min)
+    return x
+
+
 def show():
-    for ct_id, x, y, z, diam, slice_num, mask_index, z_trans in tables.iloc:
+    for ct_id, x, y, _, _, _, mask_index, z_trans, _ in tables.iloc:
         # 读入CT源文件及mask
         mask, _ = nrrd.read(os.path.join(PATH, ct_id, '{}.nrrd\{}.nrrd'.format(mask_index, mask_index)))
         ct_image = pydicom.read_file(os.path.join(PATH, ct_id, '{}.dcm'.format(z_trans + 1))).pixel_array
@@ -44,49 +51,49 @@ def show():
         plt.show()
 
 
-def maxmin_normalize(x):
-    min = np.min(x)
-    max = np.max(x)
-    x = (x - min) / (max - min)
-    return x
-
-
 def save_pictures():
-    count = 0
-    for ct_id, x, y, z, diam, slice_num, mask_index, z_trans in tables.iloc:
+    count_train = 0
+    count_val = 0
+    for ct_id, x, y, _, _, _, mask_index, z_trans, types in tables.iloc:
+        # 判断训练集和测试集
+        count = count_train if types == 'train' else count_val
+
         # 读入mask，横纵坐标调换
         mask, _ = nrrd.read(os.path.join(PATH, ct_id, '{}.nrrd\{}.nrrd'.format(mask_index, mask_index)))
         nodule_mask = np.transpose(mask[:, :, z_trans])
 
         # 裁切35×35及65×65的mask
         mask_crop = nodule_mask[round(y - 17):round(y + 18), round(x - 17):round(x + 18)]
-        imwrite('.\\dataset\\{}.mask.35.jpg'.format(count), mask_crop)
+        imwrite('dataset//%s//%d.mask.35.jpg' % (types, count), mask_crop)
         mask_crop = nodule_mask[round(y - 32):round(y + 33), round(x - 32):round(x + 33)]
 
         # 下采样到35×35
         mask_crop = np.array(Image.fromarray(mask_crop).resize((35, 35), resample=0))
-        imwrite('.\\dataset\\{}.mask.65.jpg'.format(count), mask_crop)
+        imwrite('dataset//%s//%d.mask.65.jpg' % (types, count), mask_crop)
 
         # 裁切上下三张CT图
         for i in range(-1, 2):
             # 读入CT图并归一化
-            ct_image = pydicom.read_file(os.path.join(PATH, ct_id, '{}.dcm'.format(z_trans + 1 + i))).pixel_array
+            ct_image = pydicom.read_file(os.path.join(PATH, ct_id, '%d.dcm' % (z_trans + 1 + i))).pixel_array
             ct_image[ct_image > 1200] = 1200
             ct_image[ct_image < -600] = -600
 
             # 裁切 35×35 CT图
             nodule_crop = ct_image[round(y - 17):round(y + 18), round(x - 17):round(x + 18)]
-            imwrite('.\\dataset\\{}.{}.ct.35.jpg'.format(count, i+2), maxmin_normalize(nodule_crop)*255)
+            imwrite('dataset//%s//%d.%d.ct.35.jpg' % (types, count, i+2), maxmin_normalize(nodule_crop)*255)
 
             # 裁切 65×65 CT图
             if i == 0:
                 nodule_crop = ct_image[round(y - 32):round(y + 33), round(x - 32):round(x + 33)]
                 # 下采样到35×35
                 nodule_crop = np.array(Image.fromarray(nodule_crop).resize((35, 35), resample=0))
-                imwrite('.\\dataset\\{}.{}.ct.65.jpg'.format(count, i+2), maxmin_normalize(nodule_crop)*255)
+                imwrite('dataset//%s//%d.%d.ct.65.jpg' % (types, count, i+2), maxmin_normalize(nodule_crop)*255)
 
-        print("已处理{}个结节".format(count))
-        count = count + 1
+        print("已处理%d个结节" % (count_train+count_val+1))
+        if types == 'train':
+            count_train += 1
+        else:
+            count_val += 1
 
 
 if __name__ == '__main__':
